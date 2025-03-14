1e398328d0be855ece50fdd7d4f588506580cc08
Hongkuan Zhou <tedzhouhk@gmail.com>
2025-03-14 14:23:49 -0700

    fix: indent issue (duplicated source code) (#165)

--- deploy/examples/vllm/disaggregated/kv_router.py
+++ deploy/examples/vllm/disaggregated/kv_router.py
@@ -129,20 +129,20 @@ class Router:
             print("[ROUTER] metrics.endpoint ", metrics.endpoints)
             for endpoint in metrics.endpoints:
                 worker_id = endpoint.worker_id
-            worker_metrics[worker_id] = {
-                "gpu_cache_usage_perc": endpoint.gpu_cache_usage_perc
-                if hasattr(endpoint, "gpu_cache_usage_perc")
-                else 0.0,
-                "num_requests_waiting": endpoint.num_requests_waiting
-                if hasattr(endpoint, "num_requests_waiting")
-                else 0.0,
-                "gpu_prefix_cache_hit_rate": endpoint.gpu_prefix_cache_hit_rate
-                if hasattr(endpoint, "gpu_prefix_cache_hit_rate")
-                else 0.0,
-            }
-            max_waiting = max(
-                max_waiting, worker_metrics[worker_id]["num_requests_waiting"]
-            )
+                worker_metrics[worker_id] = {
+                    "gpu_cache_usage_perc": endpoint.gpu_cache_usage_perc
+                    if hasattr(endpoint, "gpu_cache_usage_perc")
+                    else 0.0,
+                    "num_requests_waiting": endpoint.num_requests_waiting
+                    if hasattr(endpoint, "num_requests_waiting")
+                    else 0.0,
+                    "gpu_prefix_cache_hit_rate": endpoint.gpu_prefix_cache_hit_rate
+                    if hasattr(endpoint, "gpu_prefix_cache_hit_rate")
+                    else 0.0,
+                }
+                max_waiting = max(
+                    max_waiting, worker_metrics[worker_id]["num_requests_waiting"]
+                )
 
         # Get all worker IDs from the client. This is needed because scores / metrics may not have values for all workers
         # and we want all workers to be considered in the logit calculation
